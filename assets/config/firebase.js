259a69c15a6243cedec9df8a6926d5a205b5a475
noufalfakhri <nouf_alfakhri@outlook.com>
2020-09-14 17:36:52 +0300

    added config

--- assets/config/firebase.js
+++ assets/config/firebase.js
@@ -5,8 +5,8 @@ import '@firebase/firestore';
 const firebaseConfig = {
   apiKey: 'YOUR_KEY_HERE_AIzaSyAOWH',
   authDomain: 'your-auth-domain-b1234.firebaseapp.com',
-  databaseURL: 'https://your-database-name.firebaseio.com',
-  projectId: 'your-project-id-1234',
+  databaseURL: 'https://maziya.firebaseio.com',
+  projectId: '146625739252',
   storageBucket: 'your-project-id-1234.appspot.com',
   messagingSenderId: '12345-insert-yourse',
   appId: 'insert yours: 1:1234:web:ee873bd1234c0deb7eba61ce',
