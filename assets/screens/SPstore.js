45b171eb6e7ffde0b2ea1de0bd820b9885c9ac05
maather-alshablan <matheralshablan@Mathers-MacBook.local>
2020-10-08 16:54:23 +0300

    updated

--- assets/screens/SPstore.js
+++ assets/screens/SPstore.js
@@ -59,3 +59,76 @@ const Homescreen =({ navigation}) => {
         )
 }
 
+const { height } = Dimensions.get('screen');
+const height_logo = height * 0.28;
+ 
+const styless = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor : 'white'
+        
+    },
+    header: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
+        paddingTop:20
+    },
+    footer: {
+        flex: 3,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingVertical: 20,
+        paddingHorizontal: 30
+    },
+    text_header: {
+      paddingTop:100,
+        color: '#0278ae',
+        fontWeight: 'bold',
+        fontSize: 25,
+        marginLeft:150
+    },
+    text_footer: {
+        color: '#05375a',
+        fontSize: 18,
+        marginLeft:260,
+        marginTop:10
+        
+    },
+    buttom:{
+       alignItems: 'flex-end',
+        marginTop: 30
+    },
+   
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5
+ 
+    },
+    textInput: {
+        flex: 1,
+        marginTop:  -12,
+        paddingLeft: 10,
+        color: '#05375a'
+    },
+    
+    signIn: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+ 
+    textSign: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+    
+ 
+ 
+});
