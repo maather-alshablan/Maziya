a2acd8c05ec43518eb7ba51e7e922269a1cdc095
noufalfakhri <nouf_alfakhri@outlook.com>
2020-11-18 08:07:52 +0300

    added chat stack

---
+++ assets/Navigation/chatStack.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import chatRooms from '../screens/ChatRooms'
+import chatRoom from '../screens/chatRoom'
+
+function chatStack(){
+const ChatStack = createStackNavigator()
+return (
+<ChatStack.Navigator
+initialRouteName="Home"
+screenOptions={{ headerShown: false }}
+>
+
+<ChatStack.Screen name = 'Home' component= {chatRooms}
+option={{
+headerTransparent: true}} unmountOnBlur={true}/>
+ <ChatStack.Screen name= 'Room' component={chatRoom}/> 
+
+</ChatStack.Navigator>
+)}
+export default chatStack;
